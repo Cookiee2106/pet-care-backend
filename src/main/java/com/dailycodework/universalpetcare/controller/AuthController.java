7c5a9683751468770efe62922cc18c54908eabee
Cookiee2106 <daitai21062004@gmail.com>
2025-12-21 16:24:37 +0700

    Fix Auth: Return Vietnamese error message for bad credentials

--- src/main/java/com/dailycodework/universalpetcare/controller/AuthController.java
+++ src/main/java/com/dailycodework/universalpetcare/controller/AuthController.java
@@ -32,7 +32,6 @@ import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(UrlMapping.AUTH)
@@ -43,43 +42,40 @@ public class AuthController {
     private final PasswordResetService passwordResetService;
     private final ApplicationEventPublisher publisher;
 
-
-
     @PostMapping(UrlMapping.LOGIN)
     public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginRequest request) {
-        try{
-            Authentication authentication =
-                    authenticationManager
-                            .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
             SecurityContextHolder.getContext().setAuthentication(authentication);
             String jwt = jwtUtils.generateTokenForUser(authentication);
             UPCUserDetails userDetails = (UPCUserDetails) authentication.getPrincipal();
             JwtResponse jwtResponse = new JwtResponse(userDetails.getId(), jwt);
             return ResponseEntity.ok(new ApiResponse(FeedBackMessage.AUTHENTICATION_SUCCESS, jwtResponse));
 
-        }   catch (DisabledException e){
-          return ResponseEntity.status(UNAUTHORIZED).body(new ApiResponse(FeedBackMessage.ACCOUNT_DISABLED,null));
-        } catch (AuthenticationException e){
-            return ResponseEntity.status(UNAUTHORIZED).body(new ApiResponse(e.getMessage(), FeedBackMessage.INVALID_PASSWORD));
+        } catch (DisabledException e) {
+            return ResponseEntity.status(UNAUTHORIZED).body(new ApiResponse(FeedBackMessage.ACCOUNT_DISABLED, null));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(UNAUTHORIZED).body(new ApiResponse(FeedBackMessage.INVALID_PASSWORD, null));
 
         }
     }
-    
+
     @GetMapping(UrlMapping.VERIFY_EMAIL)
-    public ResponseEntity<ApiResponse> verifyEmail(@RequestParam("token")    String token) {
-        String result =   tokenService.validateToken(token);
-        return  switch (result){
+    public ResponseEntity<ApiResponse> verifyEmail(@RequestParam("token") String token) {
+        String result = tokenService.validateToken(token);
+        return switch (result) {
             case "VALID" -> ResponseEntity.ok(new ApiResponse(FeedBackMessage.VALID_VERIFICATION_TOKEN, null));
             case "VERIFIED" -> ResponseEntity.ok(new ApiResponse(FeedBackMessage.TOKEN_ALREADY_VERIFIED, null));
             case "EXPIRED" ->
-                    ResponseEntity.status(HttpStatus.GONE).body(new ApiResponse(FeedBackMessage.EXPIRED_TOKEN, null));
+                ResponseEntity.status(HttpStatus.GONE).body(new ApiResponse(FeedBackMessage.EXPIRED_TOKEN, null));
             case "INVALID" ->
-                    ResponseEntity.status(HttpStatus.GONE).body(new ApiResponse(FeedBackMessage.INVALID_VERIFICATION_TOKEN, null));
+                ResponseEntity.status(HttpStatus.GONE)
+                        .body(new ApiResponse(FeedBackMessage.INVALID_VERIFICATION_TOKEN, null));
             default -> ResponseEntity.internalServerError().body(new ApiResponse(FeedBackMessage.ERROR, null));
-            
-        } ;
-    }
 
+        };
+    }
 
     @PutMapping(UrlMapping.RESEND_VERIFICATION_TOKEN)
     public ResponseEntity<ApiResponse> resendVerificationToken(@RequestParam("token") String oldToken) {
@@ -94,36 +90,35 @@ public class AuthController {
     }
 
     @PostMapping(UrlMapping.REQUEST_PASSWORD_RESET)
-    public ResponseEntity<ApiResponse> requestPasswordReset(@RequestBody Map<String, String> requestBody){
+    public ResponseEntity<ApiResponse> requestPasswordReset(@RequestBody Map<String, String> requestBody) {
         String email = requestBody.get("email");
-        if (email == null || email.trim().isEmpty()){
+        if (email == null || email.trim().isEmpty()) {
             return ResponseEntity.badRequest()
                     .body(new ApiResponse(FeedBackMessage.INVALID_EMAIL, null));
         }
         try {
             passwordResetService.requestPasswordReset(email);
-            return ResponseEntity.
-                    ok(new ApiResponse(FeedBackMessage.PASSWORD_RESET_EMAIL_SENT, null));
-        }  catch (ResourceNotFoundException ex) {
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.PASSWORD_RESET_EMAIL_SENT, null));
+        } catch (ResourceNotFoundException ex) {
             return ResponseEntity.badRequest().body(new ApiResponse(ex.getMessage(), null));
-        }catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(new ApiResponse(e.getMessage(), null));
         }
     }
 
     @PostMapping(UrlMapping.RESET_PASSWORD)
-    public  ResponseEntity<ApiResponse> resetPassword(@RequestBody PasswordResetRequest request){
+    public ResponseEntity<ApiResponse> resetPassword(@RequestBody PasswordResetRequest request) {
         String token = request.getToken();
         String newPassword = request.getNewPassword();
-        if (token == null || token.trim().isEmpty() || newPassword == null || newPassword.trim().isEmpty()){
-            return  ResponseEntity.badRequest().body(new ApiResponse(FeedBackMessage.MISSING_PASSWORD, null));
+        if (token == null || token.trim().isEmpty() || newPassword == null || newPassword.trim().isEmpty()) {
+            return ResponseEntity.badRequest().body(new ApiResponse(FeedBackMessage.MISSING_PASSWORD, null));
         }
         Optional<User> theUser = passwordResetService.findUserByPasswordResetToken(token);
-        if (theUser.isEmpty()){
-            return  ResponseEntity.badRequest().body(new ApiResponse(FeedBackMessage.INVALID_RESET_TOKEN, null));
+        if (theUser.isEmpty()) {
+            return ResponseEntity.badRequest().body(new ApiResponse(FeedBackMessage.INVALID_RESET_TOKEN, null));
         }
         User user = theUser.get();
-        String message = passwordResetService.resetPassword(newPassword, user) ;
+        String message = passwordResetService.resetPassword(newPassword, user);
         return ResponseEntity.ok(new ApiResponse(message, null));
     }
 
