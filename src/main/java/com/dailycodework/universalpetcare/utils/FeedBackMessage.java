9382e29d3f941d200594e9b168c93eb8ef976ae0
Cookiee2106 <daitai21062004@gmail.com>
2025-12-21 16:32:32 +0700

    Fix Build: Change INVALID_PASSWORD type from Object to String

--- src/main/java/com/dailycodework/universalpetcare/utils/FeedBackMessage.java
+++ src/main/java/com/dailycodework/universalpetcare/utils/FeedBackMessage.java
@@ -2,7 +2,9 @@ package com.dailycodework.universalpetcare.utils;
 
 public class FeedBackMessage {
 
-    /*======================== Start User API=====================================*/
+    /*
+     * ======================== Start User API=====================================
+     */
     public static final String CREATE_USER_SUCCESS = "Tạo tài khoản người dùng thành công";
     public static final String DELETE_USER_SUCCESS = "Xóa tài khoản người dùng thành công";
     public static final String USER_UPDATE_SUCCESS = "Cập nhật thông tin người dùng thành công";
@@ -11,22 +13,28 @@ public class FeedBackMessage {
     public static final String NO_USER_FOUND = "Ôi không, không tìm thấy người dùng với : ";
     public static final String LOCKED_ACCOUNT_SUCCESS = "Khóa tài khoản thành công";
     public static final String UNLOCKED_ACCOUNT_SUCCESS = "Mở khóa tài khoản thành công";
-    /*======================== End User API=====================================*/
+    /* ======================== End User API===================================== */
 
-
-
-    /*======================== Start Password API=====================================*/
+    /*
+     * ======================== Start Password
+     * API=====================================
+     */
     public static final String PASSWORD_CHANGE_SUCCESS = "Đổi mật khẩu thành công! Bây giờ bạn có thể đóng biểu mẫu này.";
     public static final String PASSWORD_RESET_EMAIL_SENT = "Đã gửi liên kết đến email của bạn, vui lòng kiểm tra email để hoàn tất yêu cầu đặt lại mật khẩu";
     public static final String MISSING_PASSWORD = "Thiếu mã xác thực hoặc mật khẩu";
     public static final String INVALID_RESET_TOKEN = "Mã xác thực đặt lại mật khẩu không hợp lệ";
-    public static final String INVALID_EMAIL = "Vui lòng nhập địa chỉ email liên kết với tài khoản của bạn." ;
-    public static final String PASSWORD_RESET_SUCCESS = "Mật khẩu của bạn đã được đặt lại thành công!" ;
+    public static final String INVALID_EMAIL = "Vui lòng nhập địa chỉ email liên kết với tài khoản của bạn.";
+    public static final String PASSWORD_RESET_SUCCESS = "Mật khẩu của bạn đã được đặt lại thành công!";
 
-    /*======================== End Password API=====================================*/
+    /*
+     * ======================== End Password
+     * API=====================================
+     */
 
-
-    /*======================== Start Appointment API=====================================*/
+    /*
+     * ======================== Start Appointment
+     * API=====================================
+     */
     public static final String APPOINTMENT_UPDATE_SUCCESS = "Cập nhật lịch hẹn thành công";
     public static final String APPOINTMENT_APPROVED_SUCCESS = "Lịch hẹn đã được chấp thuận thành công";
     public static final String APPOINTMENT_DECLINED_SUCCESS = "Lịch hẹn đã bị từ chối thành công";
@@ -37,21 +45,26 @@ public class FeedBackMessage {
     public static final String APPOINTMENT_NOT_FOUND = "Không tìm thấy lịch hẹn";
     public static final String APPOINTMENT_UPDATE_NOT_ALLOWED = "Không thể cập nhật hoặc hủy lịch hẹn";
     public static final String OPERATION_NOT_ALLOWED = "Thao tác không được cho phép";
-    /*======================== End Appointment API=====================================*/
+    /*
+     * ======================== End Appointment
+     * API=====================================
+     */
 
-
-
-    /*======================== Start Pet API=====================================*/
-    public static final String PET_ADDED_SUCCESS  = "Thêm thú cưng vào lịch hẹn thành công";
+    /*
+     * ======================== Start Pet API=====================================
+     */
+    public static final String PET_ADDED_SUCCESS = "Thêm thú cưng vào lịch hẹn thành công";
     public static final String PET_UPDATE_SUCCESS = "Cập nhật thông tin thú cưng thành công";
     public static final String PET_DELETE_SUCCESS = "Xóa hồ sơ thú cưng thành công";
     public static final String PET_FOUND = "Tìm thấy thú cưng";
     public static final String PET_NOT_FOUND = "Không tìm thấy thú cưng";
 
-    /*======================== End Pet API=====================================*/
+    /* ======================== End Pet API===================================== */
 
-
-    /*======================== Start Review API=====================================*/
+    /*
+     * ======================== Start Review
+     * API=====================================
+     */
     public static final String REVIEW_NOT_ALLOWED = "Xin lỗi, chỉ những bệnh nhân đã hoàn thành lịch khám với bác sĩ này mới có thể để lại đánh giá";
     public static final String ALREADY_REVIEWED = "Bạn đã đánh giá bác sĩ này. Bạn có thể chỉnh sửa nhận xét trước đó";
     public static final String CANNOT_REVIEW = "Bác sĩ thú y không thể tự đánh giá chính mình";
@@ -62,32 +75,46 @@ public class FeedBackMessage {
     public static final String REVIEW_DELETE_SUCCESS = "Xóa đánh giá thành công";
     public static final String REVIEW_FOUND = "Tìm thấy đánh giá";
 
-
-    /*======================== End Review API=====================================*/
+    /*
+     * ======================== End Review API=====================================
+     */
     public static final String NOT_ALLOWED = "Bạn phải có một lịch khám hoàn thành với bác sĩ này để để lại đánh giá";
 
-
-    /*======================== Start General feedback =====================================*/
+    /*
+     * ======================== Start General feedback
+     * =====================================
+     */
     public static final String SUCCESS = "Thành công!";
     public static final String RESOURCE_FOUND = "Tìm thấy tài nguyên";
     public static final String SENDER_RECIPIENT_NOT_FOUND = "Không tìm thấy người gửi hoặc người nhận";
-    public static final String ERROR = "Đã xảy ra lỗi" ;
-    public static final String RESOURCE_NOT_FOUND = "Không tìm thấy tài nguyên"; ;
-    /*======================== End general feedback =====================================*/
+    public static final String ERROR = "Đã xảy ra lỗi";
+    public static final String RESOURCE_NOT_FOUND = "Không tìm thấy tài nguyên";;
+    /*
+     * ======================== End general feedback
+     * =====================================
+     */
 
-
-    /*======================== Start authentication feedback =====================================*/
-    public static final String EMPTY_PASSWORD = "Tất cả các trường là bắt buộc" ;
+    /*
+     * ======================== Start authentication feedback
+     * =====================================
+     */
+    public static final String EMPTY_PASSWORD = "Tất cả các trường là bắt buộc";
     public static final String INCORRECT_PASSWORD = "Mật khẩu không chính xác";
     public static final String PASSWORD_MISMATCH = "Xác nhận mật khẩu không khớp";
-    public static final String AUTHENTICATION_SUCCESS = "Xác thực thành công" ;
+    public static final String AUTHENTICATION_SUCCESS = "Xác thực thành công";
     public static final String ACCOUNT_DISABLED = "Xin lỗi, tài khoản của bạn đã bị vô hiệu hóa. Vui lòng liên hệ bộ phận hỗ trợ";
-    public static final Object INVALID_PASSWORD = "Tên người dùng hoặc mật khẩu không hợp lệ";
+    public static final String INVALID_PASSWORD = "Tên người dùng hoặc mật khẩu không hợp lệ";
     public static final String UNAUTHORIZED = "Vui lòng đăng nhập để gửi đánh giá.";
 
-    /*======================== End authentication feedback =====================================*/
+    /*
+     * ======================== End authentication feedback
+     * =====================================
+     */
 
-    /*======================== Start Token API =====================================*/
+    /*
+     * ======================== Start Token API
+     * =====================================
+     */
     public static final String INVALID_TOKEN = "KHÔNG HỢP LỆ";
     public static final String TOKEN_ALREADY_VERIFIED = "ĐÃ ĐƯỢC XÁC MINH";
     public static final String EXPIRED_TOKEN = "ĐÃ HẾT HẠN";
@@ -98,17 +125,27 @@ public class FeedBackMessage {
     public static final String INVALID_VERIFICATION_TOKEN = "Mã xác minh không hợp lệ";
     public static final String NEW_VERIFICATION_TOKEN_SENT = "Đã gửi một liên kết xác minh mới đến email của bạn. Vui lòng kiểm tra để hoàn tất đăng ký.";
 
-    /*======================== End Token API =====================================*/
+    /*
+     * ======================== End Token API =====================================
+     */
 
-    /*======================== Start Photo API =====================================*/
-    public static final String PHOTO_UPDATE_SUCCESS = "Cập nhật ảnh thành công"; ;
+    /*
+     * ======================== Start Photo API
+     * =====================================
+     */
+    public static final String PHOTO_UPDATE_SUCCESS = "Cập nhật ảnh thành công";;
     public static final String PHOTO_REMOVE_SUCCESS = "Xóa ảnh thành công";
-    /*======================== End Photo API =====================================*/
+    /*
+     * ======================== End Photo API =====================================
+     */
 
-
-    /*======================== Start Role API =====================================*/
+    /*
+     * ======================== Start Role API =====================================
+     */
     public static final String ROLE_NOT_FOUND = "Không tìm thấy vai trò";
 
-    /*======================== End Role API =====================================*/
+    /*
+     * ======================== End Role API =====================================
+     */
 
 }
